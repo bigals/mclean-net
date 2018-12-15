1749e7f108fd13f39f71db65e6c6dac8b0b30a24
bigals <alexander.wil.mclean@gmail.com>
2018-12-15 03:05:41 -0500

    updates for unit testing

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -20,12 +20,23 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--no-sandbox',
+          '--headless',
+          '--disable-gpu',
+          '--remote-debugging-port=9222'
+        ]
+      }
+    },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
-    singleRun: true
+    browsers: ['Chrome'],
+    singleRun: false
   });
 };
