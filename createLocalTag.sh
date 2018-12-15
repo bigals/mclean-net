7fe50519318850ac74385373b0e9b716da2d7b6b
bigals <alexander.wil.mclean@gmail.com>
2018-12-15 01:12:09 -0500

    new test

--- createLocalTag.sh
+++ createLocalTag.sh
@@ -1,8 +1,11 @@
 NPMVERSION=$(sed -nE 's/^\s*"version": "(.*?)",$/\1/p' package.json)
 GIT=`which git`
+
 ${GIT} add package.json
 ${GIT} add package-lock.json
 
 ${GIT} status
 ${GIT} commit -m "releasing version ${NPMVERSION}"
 ${GIT} tag ${NPMVERSION}
+
+exit 0
