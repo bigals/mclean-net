5edd52aba1974f2fcbbeb00e518964e73f8eeb05
bigals <alexander.wil.mclean@gmail.com>
2018-12-15 01:34:22 -0500

    fixing deploy scripts

--- mergeToRelease.sh
+++
@@ -1,6 +0,0 @@
-NPMVERSION=$(sed -nE 's/^\s*"version": "(.*?)",$/\1/p' package.json)
-
-git checkout release
-git merge ${NPMVERSION}
-
-exit 0
--- release.sh
+++ release.sh
@@ -7,10 +7,11 @@ git commit -m "releasing version ${NPMVERSION}"
 git tag ${NPMVERSION}
 
 git push
+git push origin ${NPMVERSION}
 
 git checkout release
 git merge ${NPMVERSION}
 
-git push
+git push origin ${NPMVERSION}
 
 read -p "Press enter to continue"
